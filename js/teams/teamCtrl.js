332ff356adb5f23f969442be2a246f75a64d43e5
Jon <jon@allen8r.com>
2015-02-21 13:38:43 -0700

    add toggle function in controller of step 4 - need to complete the rest of step 4

--- js/teams/teamCtrl.js
+++ js/teams/teamCtrl.js
@@ -1,5 +1,16 @@
 var app = angular.module('nbaRoutes');
 
 app.controller('teamCtrl', function($scope, $routeParams, teamService, teamData){
-
+    $scope.teamData = teamData;
+    $scope.newGame = {};
+    $scope.showNewGameForm = false;
+    
+    $scope.toggleNewGameForm = function(){
+        if (!$scope.showNewGameForm) {
+            $scope.showNewGameForm = true;
+        } else {
+            $scope.showNewGameForm = false;
+        }
+    }
+    
 });
