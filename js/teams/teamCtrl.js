66adc30cb52a0270bd45c0a96845f853d0af975f
Jon <jon@allen8r.com>
2015-02-24 18:57:58 -0700

    finish step 4

--- js/teams/teamCtrl.js
+++ js/teams/teamCtrl.js
@@ -13,4 +13,25 @@ app.controller('teamCtrl', function($scope, $routeParams, teamService, teamData)
         }
     }
     
+    if ($routeParams.team === 'utahjazz') {
+        $scope.homeTeam = 'Utah Jazz';
+        $scope.logoPath = 'images/jazz-logo.png';
+    }else if ($routeParams.team === 'losangeleslakers') {
+        $scope.homeTeam = 'Los Angeles Lakers';
+        $scope.logoPath = 'images/lakers-logo.png';
+    }else if ($routeParams.team === 'miamiheat') {
+        $scope.homeTeam = 'Miami Heat';
+        $scope.logoPath = 'images/heat-logo.png';
+    }
+    $scope.submitGame = function() {
+      $scope.homeTeam = $scope.homeTeam.split(' ').join('').toLowerCase();
+      teamService.addNewGame($scope.newGame).then(function(res) {
+          teamService.getTeamData($scope.newGame.homeTeam).then(function(res){
+              $scope.teamData = res;
+              $scope.newGame = [];
+              $scope.showNewGameForm = false;
+          });
+      });
+    };
+    
 });
